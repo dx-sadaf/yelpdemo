2265a3165ce52c5658ab9c8a66f5e9ae674175c5
Sadaf Rasheed <sadaf@discretelogix.com>
2015-02-01 10:15:41 +0500

    Specified Facebook Callback in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  
   resources :restaurants do
     collection do
       get 'search'
